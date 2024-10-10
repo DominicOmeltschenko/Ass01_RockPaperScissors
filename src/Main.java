a7364c7423bfc084c4c83d637fcb6128cc72d284
Dominic Omeltschenko <omeltsdf@mail.uc.edu>
2024-10-10 14:19:49 -0400

    code is finished and working

--- src/Main.java
+++ src/Main.java
@@ -54,43 +54,102 @@ public class Main {
                 String playerA = "";
                 String playerB = "";
                 String trash = "";
-
                 boolean done = false;
+                boolean playAgain = false;
+                String continueChoice = "Y";
+
+               do {
                 do
                 {
-                    System.out.print("What is your choice Player A, please type your choice in the form of a capital letter I.E R = Rock: ");
-                    if(in.hasNextLine())
+                    System.out.print("What is your choice Player A, please type your choice in the form of a letter I.E R = Rock: ");
+                    playerA = in.nextLine();
+                    if(playerA.equalsIgnoreCase("R") || playerA.equalsIgnoreCase("S")||playerA.equalsIgnoreCase("P"))
                     {
-                        playerA = in.nextLine();
                         done = true;
                     }
                     else
                     {
-
-                        trash = in.nextLine();
-                        System.out.println("" + trash);
-                        System.out.println("");
+                        trash = playerA;
+                        System.out.println("You said your choice was: " + trash);
+                        System.out.println("Please input a valid choice, Player A");
                         done = false;
                     }
                 }while(!done);
                 do
                 {
-                    System.out.print("What is your choice Player B, please type your choice in the form of a capital letter I.E R = Rock: ");
-                    if(in.hasNextLine())
-                    {
-                        playerB = in.nextLine();
+                    System.out.print("What is your choice Player B, please type your choice in the form of a letter I.E R = Rock: ");
+                    playerB = in.nextLine();
+                     if(playerB.equalsIgnoreCase("R") || playerB.equalsIgnoreCase("S")||playerB.equalsIgnoreCase("P"))
+                     {
                         done = true;
-                    }
-                    else
-                    {
+                         if (playerA.equalsIgnoreCase("R"))
+                         {
+                             if (playerB.equalsIgnoreCase("R"))
+                                 System.out.println("It’s a tie! No one wins.");
 
-                    trash = in.nextLine();
-                    System.out.println("" + trash);
-                    System.out.println("");
-                    done = false;
-                    }
+                             else if (playerB.equalsIgnoreCase("P"))
+                                 System.out.println("Paper covers Rock! B wins.");
+                             else if (playerB.equalsIgnoreCase("S"))
+                                 System.out.println("Rock breaks Scissors! A wins.");
+                         }
+                         if (playerA.equalsIgnoreCase("P"))
+                         {
+                             if (playerB.equalsIgnoreCase("R"))
+                                 System.out.println("Paper covers rock! A wins.");
+
+                             else if (playerB.equalsIgnoreCase("P"))
+                                 System.out.println("It’s a tie! No one wins.");
+                             else if (playerB.equalsIgnoreCase("S"))
+                                 System.out.println("Scissors Cut Paper! B wins");
+                         }
+                         if (playerA.equalsIgnoreCase("S"))
+                         {
+                             if (playerB.equalsIgnoreCase("R"))
+                                 System.out.println("Rocks Breaks Scissors! B wins.");
+
+                             else if (playerB.equalsIgnoreCase("P"))
+                                 System.out.println("Scissors Cut Paper! A wins.");
+                             else if (playerB.equalsIgnoreCase("S"))
+                                 System.out.println("It’s a tie! No one wins.");
+                         }
+
+                     }
+                     else
+                    {
+                        trash = playerB;
+                        System.out.println("You said your choice was: " + trash);
+                        System.out.println("Please input a valid choice, Player B");
+                        done = false;
+                     }
                 }while(!done);
-              System.out.println(playerA);
-              System.out.println(playerB);
+                do {
+
+
+                   System.out.println("Do you want to play again? (Y/N)");
+                   continueChoice = in.nextLine();
+                   if (continueChoice.equalsIgnoreCase("Y")||continueChoice.equalsIgnoreCase("N"))
+                   {
+                     if (continueChoice.equalsIgnoreCase("Y"))
+                     {
+                         playAgain = true;
+                         done = true;
+                     }
+                     else
+                     {
+                         playAgain = false;
+                         done = true;
+                     }
+
+                   }
+                   else
+                   {
+                       trash = continueChoice;
+                       System.out.println("You said you wanted to: " + trash);
+                       System.out.println("Please input either a Y or an N");
+                       done = false;
+                   }
+                }while(!done);
+               }while (playAgain);
+
     }
 }
